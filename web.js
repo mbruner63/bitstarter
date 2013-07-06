5add2ac73d504a004680ed291fe0c435c21412e4
Martin Bruner <marty@bruner-consulting.com>
2013-07-06 15:19:19 +0000

    changed to read index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buffy = fs.readFileSync("index.html","utf8");
+  response.send(buffy.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
